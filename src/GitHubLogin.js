059ae81a23775144bd8d6841988453dfc71b917d
ethicalGame <92223382+ethicalGame@users.noreply.github.com>
2021-10-09 21:47:17 +0530

    Update GitHubLogin.js

--- src/GitHubLogin.js
+++ src/GitHubLogin.js
@@ -36,7 +36,7 @@ class GitHubLogin extends Component {
     const popup = this.popup = PopupWindow.open(
       'github-oauth-authorize',
       `https://github.com/login/oauth/authorize?${search}`,
-      { height: 890, width: 490 }
+      { height: 900, width: 500 }
     );
 
     this.onRequest();
